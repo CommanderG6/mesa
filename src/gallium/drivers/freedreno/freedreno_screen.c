982d294401e5bd039e60b07fa8d49a4ab0a9409e
Jonathan Marek <jonathan@marek.ca>
2018-09-16 21:23:55 -0400

    the two a20x GPUs tested are a200 in the imx51 and the imx53 (not a205). the 201 id is used for the imx51 (it only has 128kb gmem as opposed to the typical 256kb for a200)
    Signed-off-by: Jonathan Marek <jonathan@marek.ca>

--- src/gallium/drivers/freedreno/freedreno_screen.c
+++ src/gallium/drivers/freedreno/freedreno_screen.c
@@ -875,6 +875,8 @@ fd_screen_create(struct fd_device *dev, struct renderonly *ro)
 	 * send a patch ;-)
 	 */
 	switch (screen->gpu_id) {
+	case 200:
+	case 201:
 	case 205:
 	case 220:
 		fd2_screen_init(pscreen);
